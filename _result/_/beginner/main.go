3a0409a980cdf3c924c015f6fceb8179e0292591
Marius Roser <mariusroser@Mariuss-MacBook-Air.local>
2022-09-23 15:40:51 +0900

    beginner

---
+++ _result/_/beginner/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, c int
+	var s string
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&s)
+	fmt.Printf("%d %s", a+b+c, s)
+	fmt.Println()
+}
