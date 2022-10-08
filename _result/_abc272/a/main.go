3633e1be4ebe3ad81b8bb5347c3b2c04e0bf58ee
Marius Roser <marius.roser@googlemail.com>
2022-10-08 14:02:29 +0200

    abc272 a

---
+++ _result/_abc272/a/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	N := scani()
+	sum := 0
+	for i := 0; i < N; i++ {
+		sum += scani()
+	}
+	fmt.Println(sum)
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}	
+
+func scani() int {
+	var x int
+	fmt.Scan(&x)
+	return x
+}
+
+func scans() string {
+	var x string
+	fmt.Scan(&x)
+	return x
+}
+
+func scanis(N int) []int{
+	arr := make([]int, 0)
+	for i := 0; i < N; i++ {
+		var tmp int
+		fmt.Scan(&tmp)
+		arr = append(arr, tmp)
+	}
+	return arr
+}
