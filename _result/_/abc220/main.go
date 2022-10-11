0d4e0b73471123267fb72825dca3aca3688a77e4
Marius Roser <marius.roser@googlemail.com>
2022-10-11 18:01:50 +0200

    abc220

---
+++ _result/_/abc220/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func main() {
+	k := scani()
+	a := scans()
+	b := scans()
+	fmt.Println(Conv(a, k) * Conv(b, k))
+}
+
+func Conv(a string, k int) int {
+	res := float64(0)
+	j := 0
+	for i := len(a) - 1; i >= 0; i-- {
+		x, _ := strconv.Atoi(string(a[i]))
+		res += math.Pow(float64(k), float64(j)) * float64(x)
+		j++
+	}
+	return int(res)
+}
+
+// ==================================================
+// init
+// ==================================================
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+
+func ni2a(n int) [][2]int {
+	a := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		a[i][0], a[i][1] = ni2()
+	}
+	return a
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func scani() int {
+	var x int
+	fmt.Scan(&x)
+	return x
+}
+
+func scans() string {
+	var x string
+	fmt.Scan(&x)
+	return x
+}
+
+func scanis(N int) []int {
+	arr := make([]int, 0)
+	for i := 0; i < N; i++ {
+		var tmp int
+		fmt.Scan(&tmp)
+		arr = append(arr, tmp)
+	}
+	return arr
+}
