950ea9de885c9a84b7015930ebfc72f8128013a4
Marius Roser <marius.roser@googlemail.com>
2022-10-12 16:47:05 +0200

    abc219 b

---
+++ _result/_abc219/b/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func main() {
+	s1 := scans()
+	s2 := scans()
+	s3 := scans()
+	a := make([]string, 0)
+	a = append(a, s1)
+	a = append(a, s2)
+	a = append(a, s3)
+	t := scans()
+	res := ""
+	for i := 0; i < len(t); i++ {
+		ind := int(t[i]-'0') - 1
+		res += a[ind]
+	}
+	fmt.Println(res)
+
+}
+
+// ==================================================
+// init
+// ==================================================
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+
+func ni2a(n int) [][2]int {
+	a := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		a[i][0], a[i][1] = ni2()
+	}
+	return a
+}
+
+func nis(arg ...int) []int {
+	n := arg[0]
+	t := 0
+	if len(arg) == 2 {
+		t = arg[1]
+	}
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni() - t
+	}
+	return a
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func scani() int {
+	var x int
+	fmt.Scan(&x)
+	return x
+}
+
+func scans() string {
+	var x string
+	fmt.Scan(&x)
+	return x
+}
+
+func scanis(N int) []int {
+	arr := make([]int, 0)
+	for i := 0; i < N; i++ {
+		var tmp int
+		fmt.Scan(&tmp)
+		arr = append(arr, tmp)
+	}
+	return arr
+}
