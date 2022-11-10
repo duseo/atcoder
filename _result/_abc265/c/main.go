0e860180a8d4f3bb5e23ac4504c7dce142cf159c
Marius Roser <marius.roser@googlemail.com>
2022-11-10 19:10:15 +0100

    abc265 c

---
+++ _result/_abc265/c/main.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func main() {
+	h := scani()
+	w := scani()
+	a := make(map[pnt]*req)
+
+	for i := 0; i < h; i++ {
+		con := scans()
+		for j := 0; j < w; j++ {
+			r := new(req)
+			r.v = false
+			r.c = string(con[j])
+			a[*makePair(i, j)] = r
+		}
+	}
+
+	x := 0
+	y := 0
+
+	cur := a[*makePair(x, y)]
+
+	for !cur.v {
+		cur.v = true
+		if cur.c == "U" && x != 0 {
+			x--
+		} else if cur.c == "D" && x != h-1 {
+			x++
+		} else if cur.c == "L" && y != 0 {
+			y--
+		} else if cur.c == "R" && y != w-1 {
+			y++
+		} else {
+			fmt.Println(x+1, y+1)
+			return
+		}
+		cur = a[*makePair(x, y)]
+	}
+	fmt.Println(-1)
+}
+
+type pnt struct {
+	x int
+	y int
+}
+
+func makePair(x, y int) *pnt {
+	p := new(pnt)
+	p.x = x
+	p.y = y
+	return p
+}
+
+type req struct {
+	c string
+	v bool
+}
+
+func rec(k int) int {
+	if k == 0 {
+		return 1
+	}
+
+	return k * rec(k-1)
+}
+
+// ==================================================
+// init
+// ==================================================
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+
+func ni2a(n int) [][2]int {
+	a := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		a[i][0], a[i][1] = ni2()
+	}
+	return a
+}
+
+func nis(arg ...int) []int {
+	n := arg[0]
+	t := 0
+	if len(arg) == 2 {
+		t = arg[1]
+	}
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni() - t
+	}
+	return a
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func scani() int {
+	var x int
+	fmt.Scan(&x)
+	return x
+}
+
+func atoi(s string) int {
+	i, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func itoa(i int) string {
+	return strconv.Itoa(i)
+}
+
+func btoi(b byte) int {
+	return atoi(string(b))
+}
+
+func scans() string {
+	var x string
+	fmt.Scan(&x)
+	return x
+}
+
+func scanis(N int) []int {
+	arr := make([]int, 0)
+	for i := 0; i < N; i++ {
+		var tmp int
+		fmt.Scan(&tmp)
+		arr = append(arr, tmp)
+	}
+	return arr
+}
+
+func printyn(b bool) {
+	if b {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	} else {
+		return a
+	}
+}
+
+func intSliceToString(x []int) string {
+	s := make([]string, 0)
+	for _, v := range x {
+		s = append(s, itoa(v))
+	}
+	return strings.Join(s, " ")
+}
