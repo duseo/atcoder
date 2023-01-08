419fa07d64b9de1e394aebd572868369c13f97fc
Marius Roser <marius.roser@googlemail.com>
2023-01-08 20:46:37 +0100

    abc272 a

--- _result/_abc272/a/main.go
+++ _result/_abc272/a/main.go
@@ -1,17 +1,89 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
 func main() {
-	N := scani()
-	sum := 0
-	for i := 0; i < N; i++ {
-		sum += scani()
+    n := ni()
+    sum := 0
+    for i := 0; i < n; i++ {
+           sum+=ni() 
+    }
+    fmt.Println(sum)
+}
+
+func rec(k int) int {
+	if k == 0 {
+		return 1
 	}
-	fmt.Println(sum)
+
+	return k * rec(k-1)
+}
+
+// ==================================================
+// init
+// ==================================================
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+
+func ni2a(n int) [][2]int {
+	a := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		a[i][0], a[i][1] = ni2()
+	}
+	return a
+}
+
+func nis(arg ...int) []int {
+	n := arg[0]
+	t := 0
+	if len(arg) == 2 {
+		t = arg[1]
+	}
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni() - t
+	}
+	return a
 }
 
 func NextPermutation(x sort.Interface) bool {
@@ -36,12 +108,28 @@ func NextPermutation(x sort.Interface) bool {
 		l--
 	}
 	return true
-}	
+}
 
 func scani() int {
 	var x int
 	fmt.Scan(&x)
 	return x
+}
+
+func atoi(s string) int {
+	i, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func itoa(i int) string {
+	return strconv.Itoa(i)
+}
+
+func btoi(b byte) int {
+	return atoi(string(b))
 }
 
 func scans() string {
@@ -50,7 +138,7 @@ func scans() string {
 	return x
 }
 
-func scanis(N int) []int{
+func scanis(N int) []int {
 	arr := make([]int, 0)
 	for i := 0; i < N; i++ {
 		var tmp int
@@ -58,4 +146,76 @@ func scanis(N int) []int{
 		arr = append(arr, tmp)
 	}
 	return arr
+}
+
+func printyn(b bool) {
+	if b {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	} else {
+		return a
+	}
+}
+
+func intSliceToString(x []int) string {
+	s := make([]string, 0)
+	for _, v := range x {
+		s = append(s, itoa(v))
+	}
+	return strings.Join(s, " ")
+}
+
+func binarySearch(l, r int, left bool) int {
+	for r-l > 1 {
+		mid := (r + l) / 2
+		if good(mid) {
+			r = mid
+		} else {
+			l = mid
+		}
+	}
+
+	if left {
+		return l
+	}
+
+	return r
+}
+
+func good(n int) bool {
+	if n > 5 {
+		return true
+	}
+	return false
+}
+
+func tdm(n,m int) [][]int {
+    tmp := make([][]int, n)
+    for i := 0; i < n; i++ {
+       tmp[i] = make([]int, m) 
+    }
+    return tmp
 }
