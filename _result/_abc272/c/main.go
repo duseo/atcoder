1f5fd12829e71fbd515ef7b36d23c5cd87a428f5
Marius Roser <marius.roser@googlemail.com>
2023-01-09 20:07:11 +0100

    abc272 c

--- _result/_abc272/c/main.go
+++ _result/_abc272/c/main.go
@@ -1,64 +1,71 @@
 package main
 
 import (
-	"fmt"
-	"sort"
 	"bufio"
+	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
 var wtr = bufio.NewWriter(os.Stdout)
-var a = make([]int, 0)
 
 func main() {
-	N := scani()
-	for i := 0; i < N; i++ {
-		tmp := scani()
-		a = append(a, tmp)
-	}
-
-	even := make([]int, 0)
-	odd := make([]int, 0)
-
-	for i := 0; i < N; i++ {
-		if a[i]%2 == 0 {
-			even = append(even, a[i])
-		} else {
-			odd = append(odd, a[i])
-		}
-	}
-
-	res := 0
-
-	sort.Ints(even)
-	sort.Ints(odd)
-	i := len(even) -1
-    j := len(odd) -1
-	maxEven := -1 
-	maxOdd := -1
-
-	if i >=1 {
-		maxEven = even[i] + even[i-1]
-	} 	
-
-	if j >=1 {
-		maxOdd = odd[j] + odd[j-1]
-	} 	
-
-	if maxEven > maxOdd {
-		res = maxEven
-	} else {
-		res = maxOdd
-	}
-
-
-	fmt.Println(res)
+    n:=ni()
+    a := nis(n)
+    sort.Ints(a)
+    so,se:=-1,-1
+    po,co,pe,ce:=-1,-1,-1,-1
+    for i := 0; i < n ; i++ {
+        tmp := a[i]
+        if tmp%2 == 0 {
+            if pe == -1 {
+                pe=tmp
+            } else if ce == -1 {
+                ce=tmp
+            } else {
+                pe=ce
+                ce=tmp
+            }
+        } else {
+            if po == -1 {
+                po=tmp
+            } else if co == -1 {
+                co=tmp
+            } else {
+                po=co
+                co=tmp
+            }
+        }
+        if pe!=-1&&ce!=-1{
+            se = pe+ce
+        } 
+        if po!=-1&&co!=-1{
+            so = po+co
+        } 
+    }
+    
+    fmt.Println(max(so,se))
 }
 
-func IsEven(x int) bool{
-	return x%2 == 0
+func rec(k int) int {
+	if k == 0 {
+		return 1
+	}
+
+	return k * rec(k-1)
+}
+
+// ==================================================
+// init
+// ==================================================
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
 }
 
 // ==================================================
@@ -94,6 +101,19 @@ func ni2a(n int) [][2]int {
 	return a
 }
 
+func nis(arg ...int) []int {
+	n := arg[0]
+	t := 0
+	if len(arg) == 2 {
+		t = arg[1]
+	}
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni() - t
+	}
+	return a
+}
 
 func NextPermutation(x sort.Interface) bool {
 	n := x.Len() - 1
@@ -117,12 +137,28 @@ func NextPermutation(x sort.Interface) bool {
 		l--
 	}
 	return true
-}	
+}
 
 func scani() int {
 	var x int
 	fmt.Scan(&x)
 	return x
+}
+
+func atoi(s string) int {
+	i, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func itoa(i int) string {
+	return strconv.Itoa(i)
+}
+
+func btoi(b byte) int {
+	return atoi(string(b))
 }
 
 func scans() string {
@@ -131,7 +167,7 @@ func scans() string {
 	return x
 }
 
-func scanis(N int) []int{
+func scanis(N int) []int {
 	arr := make([]int, 0)
 	for i := 0; i < N; i++ {
 		var tmp int
@@ -139,4 +175,76 @@ func scanis(N int) []int{
 		arr = append(arr, tmp)
 	}
 	return arr
+}
+
+func printyn(b bool) {
+	if b {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	} else {
+		return a
+	}
+}
+
+func intSliceToString(x []int) string {
+	s := make([]string, 0)
+	for _, v := range x {
+		s = append(s, itoa(v))
+	}
+	return strings.Join(s, " ")
+}
+
+func binarySearch(l, r int, left bool) int {
+	for r-l > 1 {
+		mid := (r + l) / 2
+		if good(mid) {
+			r = mid
+		} else {
+			l = mid
+		}
+	}
+
+	if left {
+		return l
+	}
+
+	return r
+}
+
+func good(n int) bool {
+	if n > 5 {
+		return true
+	}
+	return false
+}
+
+func tdm(n,m int) [][]int {
+    tmp := make([][]int, n)
+    for i := 0; i < n; i++ {
+       tmp[i] = make([]int, m) 
+    }
+    return tmp
 }
