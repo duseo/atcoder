7d8a559492e2ec02b9a5fef3a8ae1dab8de56d10
duseo <marius.roser@googlemail.com>
2023-11-02 18:02:39 +0100

    dp a

---
+++ _result/_dp/a/Program.cs
@@ -0,0 +1,9 @@
+using Library;
+
+class AtCoder
+{
+    static void Main(string[] args)
+    {
+        using var io = new IOManager(Console.OpenStandardInput(), Console.OpenStandardOutput());
+    }
+}
