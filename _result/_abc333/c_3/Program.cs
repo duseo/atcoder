74463dbc682f473befc7207c52ca98ab67661c5e
duseo <marius.roser@googlemail.com>
2023-12-17 09:31:07 +0100

    abc333 c_3

---
+++ _result/_abc333/c_3/Program.cs
@@ -0,0 +1,24 @@
+using System.Text;
+using Library;
+
+class AtCoder
+{
+    static void Main(string[] args)
+    {
+        using var io = new IOManager(Console.OpenStandardInput(), Console.OpenStandardOutput());
+
+        var l = new SortedSet<long>();
+        for (var i = new StringBuilder("1"); i.Length < 13; i.Append('1'))
+        {
+            for (var j = new StringBuilder("1"); j.Length < 13; j.Append("1"))
+            {
+                for (var k = new StringBuilder("1"); k.Length < 13; k.Append("1"))
+                {
+                    l.Add(long.Parse(i.ToString()) + long.Parse(j.ToString()) + long.Parse(k.ToString()));    
+                }
+            }
+        }
+
+        io.WriteLine(l.ElementAt(io.ReadInt()-1));
+    }
+}
