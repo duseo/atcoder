550e4e4a44f2a422c6ab5c0686727569861fda84
duseo <marius.roser@googlemail.com>
2024-01-27 13:04:36 +0100

    abc338 a

---
+++ _result/_abc338/a/Program.cs
@@ -0,0 +1,27 @@
+using System.Security.Cryptography.X509Certificates;
+using Library;
+
+class AtCoder
+{
+    static void Main(string[] args)
+    {
+        //abc148E
+        using var io = new IOManager(Console.OpenStandardInput(), Console.OpenStandardOutput());
+        var input = io.ReadString();
+        if (input[0].ToString() != input[0].ToString().ToUpper())
+        {
+            io.WriteBool(false);
+            return;
+        }
+
+        var arr = input.ToCharArray().TakeLast(input.Length-1).ToArray();
+        var s = new string(arr);
+        if (s.ToLower() != s)
+        {
+            io.WriteBool(false);
+            return;
+        }
+        
+        io.WriteBool(true);
+    }
+}
