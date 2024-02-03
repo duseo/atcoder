a72c7c31865e5b66d830e3207a59e1ccf7ad3a2e
duseo <marius.roser@googlemail.com>
2024-02-03 13:02:50 +0100

    abc339 a

---
+++ _result/_abc339/a/Program.cs
@@ -0,0 +1,10 @@
+using Library;
+
+class AtCoder
+{
+    static void Main(string[] args)
+    {
+        using var io = new IOManager(Console.OpenStandardInput(), Console.OpenStandardOutput());
+        io.WriteLine(io.ReadString().Split('.').Last());
+    }
+}
